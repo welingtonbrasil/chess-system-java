a134c31ded5c8b373cf5092fb398baa41ad3f38c
Welington Brasil <welington.brasil@outlook.com>
2022-01-12 08:14:21 -0300

    Claas Position criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Position p = new Position();
+		
+		p.setValues(2,4);
+		System.out.println(p);
 
 	}
 
---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Column ");
+		sb.append(this.column);
+		sb.append(", Row ");
+		sb.append(this.row);
+		return sb.toString();
+	}
+	
+}
