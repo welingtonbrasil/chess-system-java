d0355895ff239ebf7d4bf4404ba5c12baee3cded
Welington Brasil <welington.brasil@outlook.com>
2022-01-31 08:34:27 -0300

    implementando Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position p = new Position();
-		
-		p.setValues(2,4);
-		System.out.println(p);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
